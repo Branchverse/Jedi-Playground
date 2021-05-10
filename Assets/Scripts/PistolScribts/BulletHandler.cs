0a6b8481d137da08fe30a45f8ca849ad522e0331
Häußermann Ben <bh070@hdm-stuttgart.de>
2021-05-10 14:32:02 +0200

    hotfixed pistol and changed samplescene for demo

--- Assets/Scripts/PistolScribts/BulletHandler.cs
+++ Assets/Scripts/PistolScribts/BulletHandler.cs
@@ -5,7 +5,7 @@ using static PistolController;
 
 public class BulletHandler : MonoBehaviour
 {
-    private float bulletSpeed = 40f;
+    private float bulletSpeed = 4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,7 +31,7 @@ public class BulletHandler : MonoBehaviour
     void OnTriggerEnter()
     {
         Debug.Log("asdasd");
-        Destroy(gameObject);
+        //Destroy(gameObject);
     }
 
     // OnTrigger Destroy
