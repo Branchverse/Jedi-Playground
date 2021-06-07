07ccca4f9767a1da2f8ce7fba1b1f305ae663414
Lukas Kleinau <lk169@hdm-stuttgart.de>
2021-06-08 01:23:54 +0200

    generic script for button actions, add functionality to basic buttons, add circular drive to wheels

---
+++ Assets/Scripts/UI/GunRange/ButtonActions.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActions : MonoBehaviour
+{
+
+    public enum Usage
+    {
+        ElevatorToggle,
+        ElevatorCall,
+        RangeSaber,
+        RangeBlaster,
+        MixerStart,
+        RollingGateToggle
+    }
+
+    public enum Direction
+    {
+        none,
+        up,
+        down
+    }
+
+    public Usage UseAs;
+    public Direction direction;
+
+    public virtual void OnTrigger()
+    {
+        switch(UseAs)
+        {
+            case Usage.ElevatorToggle:
+                // check Elevator Level, move accordingly
+                break;
+
+            case Usage.ElevatorCall:
+                break;
+
+            case Usage.RangeSaber:
+                Debug.Log("Me iz SaberButton");
+                break;
+
+            case Usage.RangeBlaster:
+                Debug.Log("Me iz BlasterButton");
+                break;
+
+            case Usage.MixerStart:
+                break;
+                
+            case Usage.RollingGateToggle:
+                Debug.Log("Me iz RollingGate");
+                break;
+                /*
+                case Usage.ElevatorToggle:
+                    break;
+
+                case Usage.ElevatorToggle:
+                    break;
+                */
+
+        }
+
+
+    }
+}
