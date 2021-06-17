3ac7570d636bfa39758cd8c9c91944547bbb6a85
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-06-17 12:55:31 +0200

    Shit makes BoomBoom now in the GarbageCollector

--- Assets/Scripts/GarbageCollectorScripts/GarbageCollector.cs
+++ Assets/Scripts/GarbageCollectorScripts/GarbageCollector.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class GarbageCollector : MonoBehaviour
 {
-    public Material mat;
+    public GameObject explosion;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject + " " + other.gameObject.GetInstanceID());
-        
-        other.gameObject.AddComponent<BurnMthrFkrBURN>();
-        other.gameObject.GetComponent<BurnMthrFkrBURN>().initiateBurn(mat);
         GameObject obj = other.gameObject;
         while(obj.GetComponent<Rigidbody>() == null)
         {
             obj = obj.transform.parent.gameObject;
         }
-        Destroy(obj,3f);
+        Debug.Log($"{obj} detected and will be destroyed with a BANG");
+        Destroy(obj,1.1f);
+
+        explosion.GetComponent<ParticleSystem>().Play(true);
+        
     }
 }
