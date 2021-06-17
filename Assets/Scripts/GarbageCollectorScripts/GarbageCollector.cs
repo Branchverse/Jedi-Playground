7a1c5d1ae6579737bb463d564939bfa172f55d2f
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-06-17 15:28:27 +0200

    Sounds for explosion

--- Assets/Scripts/GarbageCollectorScripts/GarbageCollector.cs
+++ Assets/Scripts/GarbageCollectorScripts/GarbageCollector.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class GarbageCollector : MonoBehaviour
 {
     public GameObject explosion;
+    public AudioSource explosionSound;
+    private IEnumerable coroutine;
+    private GameObject obj;
 
     private void OnTriggerEnter(Collider other)
     {
-        GameObject obj = other.gameObject;
+        obj = other.gameObject;
         while(obj.GetComponent<Rigidbody>() == null)
         {
             obj = obj.transform.parent.gameObject;
         }
         Debug.Log($"{obj} detected and will be destroyed with a BANG");
-        Destroy(obj,1.1f);
-
+        StartCoroutine(Waiter());
+        Waiter();
+    }
+    
+    public IEnumerator Waiter()
+    {
+        yield return new WaitForSeconds(1f);
         explosion.GetComponent<ParticleSystem>().Play(true);
-        
+        explosionSound.Play();
+        Destroy(obj,0.05f);
     }
 }
