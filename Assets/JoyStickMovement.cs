5246536ace61d4f1552bb2b08441aa83f63864a8
coffemakingtoaster <mh340@hdm-stuttgart.de>
2021-06-18 14:00:56 +0200

    Add Elevator prefab

--- Assets/JoyStickMovement.cs
+++ Assets/JoyStickMovement.cs
@@ -10,8 +10,9 @@ public class JoyStickMovement : MonoBehaviour
     public SteamVR_Action_Vector2 JoystickInput;
     
     public float PlayerSpeed;
-
     public float PlayerGravity;
+
+    public float PlayerJiggle = 0;
 
     private CharacterController PlayerController;
 
@@ -20,7 +21,10 @@ public class JoyStickMovement : MonoBehaviour
     }
 
     void FixedUpdate(){
-        Vector3 playerOrientation = Player.instance.hmdTransform.TransformDirection(new Vector3(JoystickInput.axis.x,0,JoystickInput.axis.y));
+        Vector3 playerOrientation = Player.instance.hmdTransform.TransformDirection(new Vector3(JoystickInput.axis.x + PlayerJiggle,0,JoystickInput.axis.y+PlayerJiggle));
         PlayerController.Move(PlayerSpeed * Time.deltaTime * Vector3.ProjectOnPlane(playerOrientation, Vector3.up) - new Vector3(0,PlayerGravity,0)* Time.deltaTime);
+        if (PlayerJiggle != 0){
+            PlayerJiggle = PlayerJiggle * -1;
+        }
     }
 }
